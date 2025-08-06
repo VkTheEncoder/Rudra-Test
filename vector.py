90d1963e487aa7be0b1a8ec62b63ce28b766072d
VkTheEncoder <158612790+VkTheEncoder@users.noreply.github.com>
2025-08-06 22:41:36 +0530

    Update vector.py

--- vector.py
+++ vector.py
@@ -1,7 +1,7 @@
 # vector.py
 
 from langchain_ollama import OllamaEmbeddings
-from langchain_chroma import Chroma
+from langchain.vectorstores import Chroma
 from langchain_core.documents import Document
 import os
 import pandas as pd
